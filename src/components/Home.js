dda3ae179b639f6d2503c783b7493827659103f8
Julianb18 <julian141718@gmail.com>
2020-06-14 17:17:38 +0200

    introducing redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Pokeball from "../images/pokeball.png";
 
 class Home extends Component {
   state = {
@@ -23,8 +25,11 @@ class Home extends Component {
       posts.map((post) => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="a pokeball" />
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/" + post.id}>
+                <span className="card-title ">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
@@ -35,7 +40,7 @@ class Home extends Component {
     );
 
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
